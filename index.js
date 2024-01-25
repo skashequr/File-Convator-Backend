466012f11cab0d107e0874a7ee223dc42c0216e8
skashequr <learningwithextracare@gmail.com>
2024-01-26 04:28:33 +0600

    added all <ROUTES>: ok :)

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000 ;
 const mongoose = require('mongoose');
 const userRoutes = require("./Routes/userRoutes");
 const chatRoutes = require("./Routes/chatRoutes");
+const messageRoutes = require("./Routes/messageRoutes");
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -29,7 +30,7 @@ console.log(process.env.MONGO_URI);
 
 app.use("/user", userRoutes);
 app.use("/chat", chatRoutes);
-
+app.use("/message", messageRoutes);
 
 
 
