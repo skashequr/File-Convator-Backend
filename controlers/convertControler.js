fb7431cd358cc6e9c9756bf1a543340f2ac82c4b
skashequr <learningwithextracare@gmail.com>
2024-02-16 01:19:55 +0600

    file uplode done

--- controlers/convertControler.js
+++ controlers/convertControler.js
@@ -5,10 +5,23 @@ const fs = require('fs');
 const Docxtemplater = require('docxtemplater');
 const XlsxPopulate = require('xlsx-populate');
 const PptxGen = require('pptxgenjs');
-const officegen = require('officegen');
+
+
+
+
 const PdfToPpt = asyncHandler(async (req, res) => {
   console.log("xxyyzz");
-  
+  // const pdfBytes = fs.readFileSync("../uploads");
+  // console.log(pdfBytes);
+  // const pdfDoc = await PDFDocument.load(pdfBytes);
+
+  // const pagesText = [];
+  // for (let i = 0; i < pdfDoc.getPageCount(); i++) {
+  //   const page = pdfDoc.getPage(i);
+  //   const text = await page.getText();
+  //   pagesText.push(text);
+  // }
+  res.json({ filename: req.file.originalname });
 });
 
 module.exports = {
